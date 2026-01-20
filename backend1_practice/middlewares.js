5b905943282cb5b868540ffa9cf357dfdc0a7abb
Raghul.A.R <arraghul6@gmail.com>
2026-01-20 23:55:26 +0530

    practicing middlewares

--- backend1_practice/middlewares.js
+++ backend1_practice/middlewares.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// this is a middleware - which is called in all the methods - get post put delete
+// this is used for common middleware which is needed in all the methods which need to ne checked
+app.use(express.json());
+
+// this type of middleware is a custom middleware which we need to mention where we are
+// using it or else this will not be considered
 function userMiddleware(req, res, next) {
   const username = req.headers.username;
   const password = req.headers.password;
